78592eb7b32e0c5e61efdd5954253710c7f501b8
Alan <alan.beyersdorf@gmail.com>
2020-02-15 08:34:15 -0500

    added color producer function for elevation colors

--- map1.py
+++ map1.py
@@ -6,6 +6,14 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
 map = folium.Map(location=[48.65, -121.53], zoom_start=6) #,tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
@@ -23,7 +31,7 @@ fg = folium.FeatureGroup(name="My Map")
 for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.Marker(location=[lt, ln],
                                 popup="Elevation is " + str(el) + " meters or " + str(el*3.28084) + " feet!",
-                                icon=folium.Icon(color="green")))
+                                icon=folium.Icon(color=color_producer(el))))
 
 map.add_child(fg)
 
