7c3c779d2091c7f608c039923b5aaaafbf1da36b
Alan <alan.beyersdorf@gmail.com>
2020-02-15 18:02:57 -0500

    implemented colors based on country population

--- map1.py
+++ map1.py
@@ -35,7 +35,9 @@ for lt, ln, el in zip(lat, lon, elev):
                                 fill_color=color_producer(el),
                                 color = 'grey', fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read())))
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+    style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+    else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 map.add_child(fg)
 map.save("Map1.html")
