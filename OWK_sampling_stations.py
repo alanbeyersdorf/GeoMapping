9f059c5e7f6dcb503bc01466c7625c2f74301a68
Alan <alan.beyersdorf@gmail.com>
2020-02-17 13:18:56 -0500

    added note regarding 'engine='python''

--- OWK_sampling_stations.py
+++ OWK_sampling_stations.py
@@ -1,6 +1,8 @@
 import folium
 import pandas
 
+#throws a bunch of errors without "engine='python'"
+#found solution @ https://stackoverflow.com/questions/12468179/unicodedecodeerror-utf8-codec-cant-decode-byte-0x9c
 data = pandas.read_csv("sampling_stations.csv", engine='python')
 
 map = folium.Map(location=[41.378716, -82.509539], zoom_start=16)
